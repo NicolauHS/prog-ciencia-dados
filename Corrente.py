5c92a5783cc113d92114810ed1959dc9c3af48c4
Nícolas H. Soares <64755920+NicolauHS@users.noreply.github.com>
2024-09-20 13:08:35 -0300

    Delete Corrente.py

--- Corrente.py
+++
@@ -1,9 +0,0 @@
-from Conta import Conta
-
-
-class Corrente(Conta):
-    
-    def __init__(self, agencia, nome, data_nascimento, valor):
-        super().__init__(agencia, nome, data_nascimento, valor)
-        self.limite = valor * .1
-        pass
